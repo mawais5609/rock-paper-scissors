71f0159e3bb18408b985c5a1786e696d79ff5592
mawais5609 <mawais5609@gmail.com>
2026-02-12 00:59:00 +0500

    Completed Rock Papers Scissors only console game.

---
+++ script.js
@@ -0,0 +1,50 @@
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function getComputerChoice() {
+        const randomNumber = Math.floor(Math.random() * 3)
+
+        if (randomNumber === 0) return "rock";
+        if (randomNumber === 1) return "paper";
+        if (randomNumber === 2) return "scissors";
+    }
+
+    function getHumanChoice() {
+        return prompt("Enter your choice: rock, paper, scissors").toLowerCase();
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log(`Its a tie! Both choices are same ${humanChoice}`)
+            return;
+        }
+
+        if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ) {
+            humanScore++
+            console.log(`You Win! ${humanChoice} beats ${computerChoice} --- YourScore:${humanScore} - ComputerScore:${computerScore}`)
+        } else {
+            computerScore++
+            console.log(`You Lose! ${computerChoice} beats ${humanChoice} --- YourScore:${humanScore} - ComputerScore:${computerScore}`)
+        }
+
+    }
+
+    playRound(getHumanChoice(), getComputerChoice())
+    playRound(getHumanChoice(), getComputerChoice())
+    playRound(getHumanChoice(), getComputerChoice())
+    playRound(getHumanChoice(), getComputerChoice())
+    playRound(getHumanChoice(), getComputerChoice())
+
+    if (humanScore > computerScore) {
+        alert(`You win! YourScore:${humanScore} --- ComputerScore:${computerScore}`)
+    } else {
+        alert(`You Lose! YourScore:${humanScore} --- ComputerScore:${computerScore}`)
+    }
+}
+
+playGame()
